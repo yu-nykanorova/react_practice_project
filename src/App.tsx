1877c5756fb3d24711a2aa46ff94349018e1b628
yu-nykanorova <juliyasos@ukr.net>
2026-02-17 16:13:59 +0200

    created react functional component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import MyComponent  from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
-    <>
-    </>
-  );
+      <>
+        <MyComponent text={"Hello, my dear!"}/>
+        {MyComponent({text: "Hello again!"})}
+      </>
+  )
 }
 
-export default App;
+export default App
