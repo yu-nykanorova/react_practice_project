c335c53701e40ffc7e38cf37bf788c8b99201b4c
yu-nykanorova <juliyasos@ukr.net>
2026-02-19 11:56:58 +0200

    added missing type indication in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import {coursesAndDurationArray} from "./data/coursesAndDurationList.ts";
 import CourseAndDuration from "./components/course/CourseAndDuration.tsx";
+import type {ICourseAndDuration} from "./models/CourseAndDuration.ts";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="h-screen flex items-center justify-center bg-[url(/public/bg.jpg)] bg-cover bg-no-repeat">
       <ul className="p-6 bg-gray-50 rounded-s shadow-xl">
         {
-          coursesAndDurationArray.map((course, index) => <CourseAndDuration key={index} course={course}/>)
+          coursesAndDurationArray.map((course: ICourseAndDuration, index: number) => <CourseAndDuration key={index} course={course}/>)
         }
       </ul>
     </div>
