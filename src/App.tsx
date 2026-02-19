e97db986a5dffd36f1099fabc5826ad55c6f5689
yu-nykanorova <juliyasos@ukr.net>
2026-02-19 18:28:28 +0200

    added lesson 2 useState example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import './App.css';
+import {useState} from "react";
 
 function App() {
-
+let [counter, setCounter] = useState(0);
   return (
-    <div className="bg-amber-200">
-      Hello!
+    <div className="min-h-screen flex flex-col justify-center items-center bg-amber-100">
+        <p className="w-100 mx-auto mb-4 p-4 text-gray-700 text-4xl text-center border-2 border-solid border-indigo-500 rounded-md">{counter}</p>
+        <div className="w-100 mx-auto flex justify-between items-center gap-6">
+            <button onClick={() => {
+                setCounter(++counter);
+            }} className="px-4 py-2 bg-indigo-500 shadow-xl rounded-md text-white">Increment</button>
+            <button onClick={() => {
+                setCounter(prevState => {
+                    return --prevState;
+                });
+            }} className="px-4 py-2 bg-indigo-500 shadow-xl rounded-md text-white">Decrement</button>
+        </div>
     </div>
   );
 }
