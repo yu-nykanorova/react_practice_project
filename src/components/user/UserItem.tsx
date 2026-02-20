66594fd6292e7890f9a7ef76dc553c463de70aae
yu-nykanorova <juliyasos@ukr.net>
2026-02-20 17:15:39 +0200

    added example of state lifting

--- src/components/user/UserItem.tsx
+++ src/components/user/UserItem.tsx
@@ -2,10 +2,16 @@ import type {User} from "../../models/userModel.ts";
 
 type UserItemProps = {
     user: User;
+    foo: (item: User) => void;
 }
 
-export const UserItem = ({user}: UserItemProps) => {
+export const UserItem = ({user, foo}: UserItemProps) => {
     return (
-        <h2 className="text-gray-600 text-3xl duration-300 ease-in-out cursor-pointer hover:text-blue-700">{user.name}</h2>
+        <li className="flex justify-between items-cente gap-2">
+            <h2 className="text-gray-600 text-3xl duration-300 ease-in-out cursor-pointer hover:text-blue-700">{user.name}</h2>
+            <button onClick={() => {
+                foo(user);
+            }} className="px-2 py-1 border-solid border-2 border-indigo-500 rounded-xs duration-300 ease-in-out hover:bg-white">Details</button>
+        </li>
     );
 };
--- src/components/user/UsersList.tsx
+++ src/components/user/UsersList.tsx
@@ -5,18 +5,36 @@ import {getUsers} from "../../services/api.service.ts";
 
 export const UsersList = () => {
     const [users, setUsers] = useState<User[]>([]);
+    const [item, setItem] = useState<User | null>(null);
 
     useEffect(() => {
         getUsers().then(data => setUsers(data));
     }, []);
 
+    const foo = (item: User): void => {
+        setItem(item);
+    }
+
     return (
-        <>
+        <div className="w-200">
+            <ul className="mb-8 flex flex-col gap-2">
+                {
+                    users.map((user) => (
+                        <UserItem foo={foo} key={user.id} user={user}/>
+                    ))
+                }
+            </ul>
+            <h3 className="mb-4 text-2xl">User info</h3>
             {
-                users.map((user) => (
-                    <UserItem key={user.id} user={user}/>
-                ))
+                !item ?
+                    <p className="text-xl">Click button "Details" to view user contacts</p>
+                    :
+                   <div className="text-xl">
+                        <p className="font-semibold">Email: <span className="font-normal">{item.email}</span></p>
+                        <p className="font-semibold">Phone: <span className="font-normal">{item.phone}</span></p>
+                        <p className="font-semibold">Website: <span className="font-normal">{item.website}</span></p>
+                    </div>
             }
-        </>
+        </div>
     );
 };
