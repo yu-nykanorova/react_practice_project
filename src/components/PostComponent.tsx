29935f8ef7372ce96167e919de864f4afddae8e7
yu-nykanorova <juliyasos@ukr.net>
2026-02-21 10:59:24 +0200

    added text-styles

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -1,4 +1,4 @@
-import type {FC} from "react";
+import {type FC} from "react";
 import type {IPost} from "../models/postModel.ts";
 
 type PostComponentProps = {
@@ -7,11 +7,11 @@ type PostComponentProps = {
 
 export const PostComponent: FC<PostComponentProps> = ({post}) => {
     return (
-        <li className="grid grid-rows-[12%_12%_100px_200px] p-4 bg-gray-100">
+        <li className="flex flex-col p-4 bg-gray-100">
             <p className="text-lg font-semibold">Post - {post.id}</p>
             <p className="text-lg font-semibold text-red-800">User #{post.userId}</p>
-            <h2 className="mb-4 mt-4 text-green-800 text-2xl font-semibold">{(post.title)[0].toUpperCase() + (post.title).slice(1)}</h2>
-            <p className="text-lg">{(post.body)[0].toUpperCase() + (post.body).slice(1)}</p>
+            <h2 className="min-h-20 mb-4 mt-4 text-green-800 text-2xl font-semibold">{(post.title)[0].toUpperCase() + (post.title).slice(1)}</h2>
+            <p className="text-lg leading-6">{(post.body)[0].toUpperCase() + (post.body).slice(1)}</p>
         </li>
     );
 };
--- src/components/PostComponents.tsx
+++ src/components/PostComponents.tsx
@@ -8,12 +8,12 @@ export const PostComponents = () => {
 
     useEffect(() => {
         getPosts().then(data => setPostList(data));
-    },[])
+    },[]);
 
     return (
         <>
             <h1 className="mb-8 text-3xl text-center text-white font-semibold">POSTS LIST</h1>
-            <ul className="max-w-300 mx-auto grid grid-cols-2 gap-8">
+            <ul className="max-w-300 mx-auto grid grid-cols-2 auto-rows-[minmax(300px,auto)] gap-8">
                 {
                     postList.map((item) => (
                         <PostComponent key={item.id} post={item}/>
