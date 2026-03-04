29c04698666153408e19006e4e61739ff5199ce1
yu-nykanorova <juliyasos@ukr.net>
2026-03-04 13:00:24 +0200

    changed Link to NavLink in menu components and added styles for active state

--- src/components/menu/SourceMenu.tsx
+++ src/components/menu/SourceMenu.tsx
@@ -1,20 +1,28 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {ArrowRight} from "../ui/ArrowRight.tsx";
 
 export const SourceMenu = () => {
+
+    const menuItemClass = ({isActive}: {isActive: boolean}) => (
+        `
+            px-4 py-2 flex items-center justify-between gap-2 bg-gray-50 text-xl rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-purple-200 shadow-md
+            ${isActive ? "bg-purple-200 font-semibold" : ""}
+        `
+    );
+
     return (
         <ul className="max-w-1/3 mx-auto mt-4 mb-6 flex flex-col gap-2">
-            <li className="px-4 py-2 bg-gray-50 text-xl rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-purple-200 shadow-md">
-                <Link to={"jsonplaceholder"} className="flex items-center justify-between gap-2" >
+            <li>
+                <NavLink to={"jsonplaceholder"} className={menuItemClass} >
                     JSONPlaceholder
                     <ArrowRight/>
-                </Link>
+                </NavLink>
             </li>
-            <li className="px-4 py-2 bg-gray-50 text-xl rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-purple-200 shadow-md">
-                <Link to={"dummyjson"} className="flex items-center justify-between gap-2">
+            <li>
+                <NavLink to={"dummyjson"} className={menuItemClass}>
                     DummyJSON
                     <ArrowRight/>
-                </Link>
+                </NavLink>
             </li>
         </ul>
     );
