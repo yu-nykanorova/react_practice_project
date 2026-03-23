3f944508784192bdcc5083aac1caa56c38da8e64
yu-nykanorova <juliyasos@ukr.net>
2026-03-23 22:29:49 +0200

    added catching errors into userSlice.ts

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -5,19 +5,21 @@ import {useAppDispatch} from "../redux/hooks/useAppDispatch.tsx";
 import {Link} from "react-router-dom";
 
 export const UsersPage = () => {
-    const {users, loadState} = useAppSelector(({userSlice}) => userSlice);
+    const {users, loadState, error} = useAppSelector(({userSlice}) => userSlice);
     //const users = useAppSelector(state => state.userSlice.users);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        dispatch(userSliceActions.loadUsers());
+        if (users.length === 0) {
+            dispatch(userSliceActions.loadUsers());
+        }
     }, []);
 
     return (
         <ul>
-            {!loadState && <div>Loading</div>}
-
-            {
+            {loadState === "loading" && <div>Loading</div>}
+            {loadState === "failed" && <div>{error}</div>}
+                        {
                 users.map((user) => (
                     <Link className="block" to={`/users/${user.id}`} key={user.id}>{user.name}</Link>
                 ))
--- src/redux/slices/userSlice/userSlice.ts
+++ src/redux/slices/userSlice/userSlice.ts
@@ -1,30 +1,33 @@
 import type {IUser} from "../../../models/IUser.ts";
-import {createAsyncThunk, createSlice, isFulfilled, isRejected, type PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, isFulfilled, isPending, isRejected, type PayloadAction} from "@reduxjs/toolkit";
 
 // опис типу частини стейту, якрю є слайс, це структура даних для userSlice
 type UserSliceType = {
     users: IUser[];
     user: IUser | null;
-    loadState: boolean;
+    loadState: "succeed" | "loading" | "failed";
+    error: string | null;
 };
 
 // початкове значення state
 // при першому запуску store буде {users: []}
-const initialState: UserSliceType = {users: [], user: null, loadState: false};
+const initialState: UserSliceType = {users: [], user: null, loadState: "succeed", error: null};
 
 const loadUsers = createAsyncThunk(
     "userSlice/loadUsers",
     async (_, thunkAPI) => {
         try {
-            const users = await fetch("https://jsonplaceholder.typicode.com/users")
-                .then(res => res.json());
+            const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
-            //thunkAPI.dispatch(userSliceActions.changeLoadState(true));
-
-            return thunkAPI.fulfillWithValue(users);
-            //throw new Error();
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
+            if (!res.ok) {
+                return thunkAPI.rejectWithValue("Server error");
+            }
+            return await res.json();
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                return thunkAPI.rejectWithValue(error.message);
+            }
+            return thunkAPI.rejectWithValue("Unknown error");
         }
     }
 );
@@ -34,15 +37,16 @@ const loadUser = createAsyncThunk(
     "userSlice/loadUser",
     async (id: string, thunkAPI) => {
         try {
-            const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-                .then(res => res.json());
-
-            //thunkAPI.dispatch(userSliceActions.changeLoadState(true));
-
-            return thunkAPI.fulfillWithValue(user);
-            //throw new Error();
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
+            const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            if (!res.ok) {
+                return thunkAPI.rejectWithValue("Server error");
+            }
+            return await res.json();
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                return thunkAPI.rejectWithValue(error.message);
+            }
+            return thunkAPI.rejectWithValue("Unknown error");
         }
     }
 );
@@ -54,29 +58,27 @@ export const userSlice = createSlice({
     // reducers - опис функцій, які змінюють state
     // У звичайному Redux не можна мутувати state, але Redux Toolkit використовує бібліотеку Immer, яка робить іммутабельність
     // action:PayloadAction<IUser[]> - це об'єкт, який приходить при dispatch
-    reducers: {
-        changeLoadState: (state, action: PayloadAction<boolean>) => {
-            state.loadState = action.payload;
-        }
-    },
+    reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(loadUsers.fulfilled, (state, action: PayloadAction<IUser[]>) => {
-                state.users = action.payload
-            })
-            .addCase(loadUsers.rejected, (state, action) => {
-                console.log(state);
-                console.log(action);
+                state.users = action.payload;
             })
             .addCase(loadUser.fulfilled, (state, action: PayloadAction<IUser>) => {
-                state.user = action.payload
+                state.user = action.payload;
+            })
+            .addMatcher(isPending(loadUser, loadUsers), (state) => {
+                state.loadState = "loading";
+                state.error = null;
+                state.user = null;
             })
             .addMatcher(isFulfilled(loadUser, loadUsers), (state) => {
-                state.loadState = true;
+                state.loadState = "succeed";
             })
-            .addMatcher(isRejected(loadUser, loadUsers), (state) => {
-                console.log(state);
-            })
+            .addMatcher(isRejected(loadUser, loadUsers), (state, action) => {
+                state.loadState = "failed";
+                state.error = action.payload as string ?? "Error";
+            });
     }
 });
 
